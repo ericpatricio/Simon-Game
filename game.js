673e336d63e271d29a691c52134ddbf34cbb64b2
ericpatricio <ericwpatricio@gmail.com>
2024-02-07 00:07:26 -0500

    Add flash animation game.js

--- game.js
+++ game.js
@@ -2,12 +2,17 @@
 const buttonColor = ["red", "blue", "green", "yellow"];
 // chosen colors
 let gamePattern = [];
-
+console.log(gamePattern);
 // Function generate random number between 0 and 3.
 function nextSequence() {
   const randomNumber = Math.floor(Math.random() * 4);
 
   let randomChosenColor = buttonColor[randomNumber];
+
   gamePattern.push(randomChosenColor);
+  // jQuery to select the button with the same id to animate a flash to the button selected
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
 }
 nextSequence();
